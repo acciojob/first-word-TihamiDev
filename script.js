24e99c43de7d2fedebcd84c1f19fe4da2f78a923
Tihami Rahman <138518399+TihamiDev@users.noreply.github.com>
2026-05-20 15:38:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 function firstWord(s) {
   // your code here
+	let newS = "";
+	for (let i = 0; i < s.length; i++) {
+		let currChar = s[i];
+		if(currChar == " "){
+			break;
+		}
+			newS = newS + currChar;
+	}
+	return newS
 }
-
+function firstWord(s) {
+	  return s.split(" ")[0];
+}
 // Do not change the code below
 
 const s = prompt("Enter String:");
