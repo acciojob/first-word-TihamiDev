7a2c04b36e641fd2d092e1478a3d3dd0e0e62979
Tihami Rahman <138518399+TihamiDev@users.noreply.github.com>
2026-05-20 15:44:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-function firstWord(s) {
-  // your code here
-	let newS = "";
-	for (let i = 0; i < s.length; i++) {
-		let currChar = s[i];
-		if(currChar == " "){
-			break;
-		}
-			newS = newS + currChar;
-	}
-	return newS
-}
+// function firstWord(s) {
+//   // your code here
+// 	let newS = "";
+// 	for (let i = 0; i < s.length; i++) {
+// 		let currChar = s[i];
+// 		if(currChar == " "){
+// 			break;
+// 		}
+// 			newS = newS + currChar;
+// 	}
+// 	return newS
+// }
 function firstWord(s) {
 	  return s.split(" ")[0];
 }
